21ae615b0a0d926114fbf9daefc59a39efedfe27
jon <jon@jons-MacBook-Pro.local>
2020-10-19 18:36:00 -0500

    Updates

---
+++ precache-manifest.9ea177b834f8cdf24fb30bde2882241a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6223631d0d5de6f3cacbc8035531eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "507cb275bba5dc54f1e0",
+    "url": "/static/css/main.2b0bfc16.chunk.css"
+  },
+  {
+    "revision": "92f000bf0496a06d4548",
+    "url": "/static/js/2.bc14cdd9.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.bc14cdd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507cb275bba5dc54f1e0",
+    "url": "/static/js/main.0cd4f34f.chunk.js"
+  },
+  {
+    "revision": "df50e106388b891167c1",
+    "url": "/static/js/runtime-main.07ac94ce.js"
+  },
+  {
+    "revision": "15f6ec091f0f274939a07e85778d5796",
+    "url": "/static/media/jony-closer.15f6ec09.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jonyalmeida/jonyalmeida.github.io.git/precache-manifest.49a05a2592b956989560fe30afe62c1d.js"
+  "/precache-manifest.9ea177b834f8cdf24fb30bde2882241a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jonyalmeida/jonyalmeida.github.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
