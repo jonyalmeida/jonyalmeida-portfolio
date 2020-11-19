9314319cec8fbfb41c5aa90ed51fa4685b9207be
jon <jon@jons-MacBook-Pro.local>
2020-11-18 18:39:03 -0600

    change li to div

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -6,8 +6,8 @@ export default function Skill({ skillObj }) {
         <div>
             <div className='skill'>
                 {skillObj.skillItems.map((item, idx) => (
-                    <ul key={idx}>
-                        <li className='skill--item-name'>{item.name}</li>
+                    <div key={idx}>
+                        <div className='skill--item-name'>{item.name}</div>
                         <div className='skill--logo-bar'>
                             <img
                                 src={item.logoURL}
@@ -21,7 +21,7 @@ export default function Skill({ skillObj }) {
                         {item.other ? (
                             <li className='skill--item-other'>{item.other}</li>
                         ) : null}
-                    </ul>
+                    </div>
                 ))}
             </div>
         </div>
