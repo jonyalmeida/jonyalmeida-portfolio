f22e0fccd474e347906ecda2d52e973a0c66eaba
jon <jon@jons-MacBook-Pro.local>
2020-12-05 22:12:30 -0600

    changed background

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,86 +35,89 @@ export default function Home() {
         });
     };
 
-    return (
-        <div className='home' id='hm'>
-            <div className='home--personal'>
-                <div>
-                    <h2>Full Stack Software Engineer-</h2>
-                    <h3>
-                        Mindful in life. Agile at work. Passion for solving
-                        problems.
-                    </h3>
-                </div>
-                <h2>Contact:</h2>
-                <div className='home--personal--links'>
-                    <a href='mailto:jony.figs@gmail.com'>
-                        <img
-                            style={{ width: "95px" }}
-                            className='home--personal-github'
-                            src={emailLogo}
-                            alt='email'
-                        />
-                    </a>
-                    <a href='callto:jony.figs@gmail.com'>
-                        <img
-                            className='home--personal-github'
-                            src={phoneLogo}
-                            alt='phone'
-                        />
-                    </a>
-                    <br />
-                    <br />
-                    <a href='https://github.com/jonyalmeida'>
-                        <img
-                            className='home--personal-github'
-                            src={githubLogo}
-                            alt='github'
-                        />
-                    </a>
-                    <br />
-                    <br />
-                    <a href='https://www.linkedin.com/in/jony-almeida-48940a16b/'>
-                        <img
-                            className='home--personal-github'
-                            src={linkedinLogo}
-                            alt='linkedin'
-                        />
-                    </a>
-                    <br />
-                    <br />
-                    <a href='https://www.codewars.com/users/hannahou'>
-                        <img
-                            className='home--personal-github'
-                            src={codewarsLogo}
-                            alt='codewars'
-                        />
-                    </a>
-                </div>
-            </div>
-            <div className='home--skills'>
-                {/* <SkillNav handleClick={handleClick} /> */}
-                <div className='carousel'>
-                    <div className='carousel--side-div'>
-                        {select.left >= 0 && (
-                            <Skill
-                                id='left'
-                                skillObj={skills[navArray[select.left]]}
-                            />
-                        )}
-                    </div>
-                    <div id='main' className='carousel--main-div'>
-                        <Skill skillObj={skills[navArray[select.selected]]} />
-                    </div>
-                    <div id='right' className='carousel--side-div'>
-                        {select.right < 4 && (
-                            <Skill
-                                id='right'
-                                skillObj={skills[navArray[select.right]]}
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    return <div className='canvas'>
+        <canvas id="canvas" width="2098" height="1956" style={{ width: '1049px', height: '978px' }}></canvas>
+    </div>
+    // return (
+    //     <div className='home' id='hm'>
+    //         <div className='home--personal'>
+    //             <div>
+    //                 <h2>Full Stack Software Engineer-</h2>
+    //                 <h3>
+    //                     Mindful in life. Agile at work. Passion for solving
+    //                     problems.
+    //                 </h3>
+    //             </div>
+    //             <h2>Contact:</h2>
+    //             <div className='home--personal--links'>
+    //                 <a href='mailto:jony.figs@gmail.com'>
+    //                     <img
+    //                         style={{ width: "95px" }}
+    //                         className='home--personal-github'
+    //                         src={emailLogo}
+    //                         alt='email'
+    //                     />
+    //                 </a>
+    //                 <a href='callto:jony.figs@gmail.com'>
+    //                     <img
+    //                         className='home--personal-github'
+    //                         src={phoneLogo}
+    //                         alt='phone'
+    //                     />
+    //                 </a>
+    //                 <br />
+    //                 <br />
+    //                 <a href='https://github.com/jonyalmeida'>
+    //                     <img
+    //                         className='home--personal-github'
+    //                         src={githubLogo}
+    //                         alt='github'
+    //                     />
+    //                 </a>
+    //                 <br />
+    //                 <br />
+    //                 <a href='https://www.linkedin.com/in/jony-almeida-48940a16b/'>
+    //                     <img
+    //                         className='home--personal-github'
+    //                         src={linkedinLogo}
+    //                         alt='linkedin'
+    //                     />
+    //                 </a>
+    //                 <br />
+    //                 <br />
+    //                 <a href='https://www.codewars.com/users/hannahou'>
+    //                     <img
+    //                         className='home--personal-github'
+    //                         src={codewarsLogo}
+    //                         alt='codewars'
+    //                     />
+    //                 </a>
+    //             </div>
+    //         </div>
+    //         <div className='home--skills'>
+    //             {/* <SkillNav handleClick={handleClick} /> */}
+    //             <div className='carousel'>
+    //                 <div className='carousel--main-div'>
+    //                     {select.left >= 0 && (
+    //                         <Skill
+    //                             id='left'
+    //                             skillObj={skills[navArray[select.left]]}
+    //                         />
+    //                     )}
+    //                 </div>
+    //                 <div id='main' className='carousel--main-div'>
+    //                     <Skill skillObj={skills[navArray[select.selected]]} />
+    //                 </div>
+    //                 <div id='right' className='carousel--main-div'>
+    //                     {select.right < 4 && (
+    //                         <Skill
+    //                             id='right'
+    //                             skillObj={skills[navArray[select.right]]}
+    //                         />
+    //                     )}
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 }
